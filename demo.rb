c6e748d0d5493e55c33e40ba924bc311d0dcf732
Artur <amrozinski@gmail.com>
2020-10-27 14:53:23 +0000

    updated working

--- demo.rb
+++ demo.rb
@@ -17,7 +17,7 @@ POSTCODES = [' TN1',  ' TN2', ' TN3', ' TN4', " TN9", ' TN10',' TN11',' TN12',
              ' TN13', ' TN14',' TN15',' TN17',' TN18',' TN19',' TN23',' TN24',
              ' TN25', ' TN26',' TN27',' ME1', ' ME2', ' ME3', ' ME4', ' ME5',
              ' ME6',  ' ME7', ' ME8', ' ME9', ' ME10',' ME11',' ME12',' ME13',
-             ' ME14', ' ME15',' ME16',' ME17',' ME18',' ME19',' ME20',' CT1'
+             ' ME14', ' ME15',' ME16',' ME17',' ME18',' ME19',' ME20',' CT1',
              ' CT2',  ' CT3', ' CT4',  'SE10', 'SE3', ' SE18', 'E14', ' GATWICK']
 
 loop do
